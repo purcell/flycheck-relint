12c155a42f46d387e6e8bc3a1f1da7a6cb41575b
Steve Purcell <steve@sanityinc.com>
2020-02-22 17:33:22 +1300

    Add URL header

--- flycheck-relint.el
+++ flycheck-relint.el
@@ -5,6 +5,7 @@
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;; Keywords: lisp
 ;; Version: 0
+;; URL: https://github.com/purcell/flycheck-relint
 ;; Package-Requires: ((emacs "24.4") (flycheck "0.22") (relint "1.13"))
 
 ;; This program is free software; you can redistribute it and/or modify
