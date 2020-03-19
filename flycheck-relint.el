c74ba5a7d15e216d680e48ad07a2cc6bb88cf1b7
Steve Purcell <steve@sanityinc.com>
2020-03-20 12:06:01 +1300

    Adapt to relint 1.15
    Fixes #2

--- flycheck-relint.el
+++ flycheck-relint.el
@@ -4,9 +4,9 @@
 
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;; Keywords: lisp
-;; Version: 0
+;; Package-Version: 0-pre
 ;; URL: https://github.com/purcell/flycheck-relint
-;; Package-Requires: ((emacs "26.1") (flycheck "0.22") (relint "1.14"))
+;; Package-Requires: ((emacs "26.1") (flycheck "0.22") (relint "1.15"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -42,9 +42,9 @@
   "Flycheck start function for relint.
 CHECKER is this checker, and CALLBACK is the flycheck dispatch function."
   (funcall callback 'finished
-           (mapcar (pcase-lambda (`(,message ,expr-pos ,error-pos ,str ,str-idx))
+           (mapcar (pcase-lambda (`(,message ,expr-pos ,error-pos ,str ,str-idx ,severity))
                      (if error-pos
-                         (flycheck-relint--error-at error-pos message)
+                         (flycheck-relint--error-at severity error-pos message)
                        (flycheck-relint--error-at expr-pos
                                                   (mapconcat 'identity
                                                              (append (list message (relint--quote-string str))
@@ -53,14 +53,14 @@ CHECKER is this checker, and CALLBACK is the flycheck dispatch function."
                    (relint-buffer (current-buffer)))))
 
 
-(defun flycheck-relint--error-at (pos message)
-  "Create a flycheck error with MESSAGE for POS."
+(defun flycheck-relint--error-at (pos severity message)
+  "Create a flycheck error with MESSAGE and SEVERITY for POS."
   (save-excursion
     (goto-char pos)
     (move-beginning-of-line 1)
     (let* ((line (line-number-at-pos))
            (col (- pos (point))))
-      (flycheck-error-new-at line (1+ col) 'error message :checker 'flycheck-relint))))
+      (flycheck-error-new-at line (1+ col) severity message :checker 'flycheck-relint))))
 
 
 
